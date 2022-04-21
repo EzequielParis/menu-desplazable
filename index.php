03a1b9cab52c090d59179e8a39a698027bfd56d5
rafael <rafaelnunes2299@gmail.com>
2022-04-21 18:52:33 -0300

    commit 2

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $variable = "Valor"
+    for ($i=0; $i < 10; $i++) { 
+        echo "<b> Hola mundo </br>" . "<br/>";
+        echo "<b> Hola mundo </br>" . "<br/>";
+    }
+
+    ?>
+</body>
+</html>
